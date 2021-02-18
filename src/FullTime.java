c1d36c432428083c5a7356627dec74751d862a0e
Ashish Shenoy <61940795+ashenoy20@users.noreply.github.com>
2021-02-17 22:54:56 -0500

    Update FullTime.java

--- src/FullTime.java
+++ src/FullTime.java
@@ -1,26 +1,36 @@
 public class FullTime extends Employee{
     private double salary;
 
-    private static final int TOTAL_PAY_PERIODS = 26;
+    public static final int TOTAL_PAY_PERIODS = 26;
 
     public FullTime(Profile profile, double salary) {
-        super(profile, "FULL TIME");
+        super(profile);
         this.salary = salary;
+    }
+
+    public double getSalary(){
+        return this.salary;
     }
 
     @Override
     public void calculatePayment() {
         double payment = salary/TOTAL_PAY_PERIODS;
-
+        super.setPayment(payment);
     }
 
     @Override
     public String toString() {
-        return super.toString();
+        return super.toString() + "::FULL TIME::Annual Salary $" + this.salary;
     }
 
     @Override
     public boolean equals(Object obj) {
-        return super.equals(obj);
+        FullTime number1 = this;
+        FullTime number2 = (FullTime) obj;
+
+        boolean equalProfile = super.equals(number2);
+        boolean equalSalary = number1.salary == number2.salary;
+
+        return equalProfile && equalSalary;
     }
 }

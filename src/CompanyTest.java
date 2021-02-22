0d1f1a48440eebf34bae61a2933d1db9ec6fbad4
Abimanyu Ananthu <30740112+ab1manyu@users.noreply.github.com>
2021-02-22 14:54:51 -0500

    Update CompanyTest.java

--- src/CompanyTest.java
+++ src/CompanyTest.java
@@ -1,1 +1,30 @@
-import org.junit.jupiter.api.BeforeEach;import static org.junit.jupiter.api.Assertions.*;// hiclass CompanyTest {    Profile profile;    Employee employee;    Company company;    @BeforeEach    void setUp() {        profile = new Profile("Over,Bend","CS", new Date());        employee = new Employee(profile);        company = new Company();    }    @org.junit.jupiter.api.Test    void add() {        assertTrue(company.add(employee));        assertFalse(company.add(employee));    }    @org.junit.jupiter.api.Test    void remove() {    }    @org.junit.jupiter.api.Test    void setHours() {    }}
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompanyTest {
+    Profile profile;
+    Employee employee;
+    Company company;
+
+    @BeforeEach
+    void setUp() {
+        profile = new Profile("Over,Bend","CS", new Date());
+        employee = new Employee(profile);
+        company = new Company();
+    }
+
+    @org.junit.jupiter.api.Test
+    void add() {
+        assertTrue(company.add(employee));
+        assertFalse(company.add(employee));
+    }
+
+    @org.junit.jupiter.api.Test
+    void remove() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setHours() {
+    }
+}

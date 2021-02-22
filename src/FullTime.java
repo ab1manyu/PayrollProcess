d5c987432f91d0435d1540707b7cc31755753546
Abimanyu Ananthu <30740112+ab1manyu@users.noreply.github.com>
2021-02-22 16:17:51 -0500

    Update FullTime.java

--- src/FullTime.java
+++ src/FullTime.java
@@ -1,1 +1,35 @@
-import java.text.DecimalFormat;// hipublic class FullTime extends Employee{    public static final int TOTAL_PAY_PERIODS = 26;    private double salary;    public FullTime(Profile profile, double salary) {        super(profile);        this.salary = salary;    }    public double getSalary(){        return this.salary;    }    @Override    public void calculatePayment() {        double payment = salary/TOTAL_PAY_PERIODS;        super.setPayment(payment);    }    @Override    public String toString() {        DecimalFormat df = new DecimalFormat("$#,##0.00");        return super.toString() + "::FULL TIME::Annual Salary " + df.format(this.salary);    }    @Override    public boolean equals(Object obj) {        if(obj instanceof FullTime){            return super.equals(obj);        }        return false;    }}
+import java.text.DecimalFormat; 
+
+public class FullTime extends Employee{
+    public static final int TOTAL_PAY_PERIODS = 26;
+    private double salary;
+
+    public FullTime(Profile profile, double salary) {
+        super(profile);
+        this.salary = salary;
+    }
+
+    public double getSalary(){
+        return this.salary;
+    }
+
+    @Override
+    public void calculatePayment() {
+        double payment = salary/TOTAL_PAY_PERIODS;
+        super.setPayment(payment);
+    }
+
+    @Override
+    public String toString() {
+        DecimalFormat df = new DecimalFormat("$#,##0.00");
+        return super.toString() + "::FULL TIME::Annual Salary " + df.format(this.salary);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof FullTime){
+            return super.equals(obj);
+        }
+        return false;
+    }
+}
